fd132af1fc20286755fe8f7346f1fafa9468f1c7
Bruno Fulber Wide <bfwide07@gmail.com>
2022-07-28 17:35:42 -0300

    added forEach to DSPicker

--- Sources/FellasDS/AtomicDesign/Organisms/DSPicker.swift
+++ Sources/FellasDS/AtomicDesign/Organisms/DSPicker.swift
@@ -11,19 +11,22 @@ import Combine
 
 public struct DSPicker<Content: View, ID: Hashable>: View {
     
-    @State var vm: DSPickerSelectionViewModel
+    @StateObject var vm: DSPickerSelectionViewModel = DSPickerSelectionViewModel()
+    @Binding var selection: ID?
     public var content: () -> Content
     
     public init(selection: Binding<ID?>,
          @DSPickerBuilder content: @escaping () -> Content) {
-        
-        self.vm = DSPickerSelectionViewModel()
+        self._selection = selection
         self.content = content
     }
 
     public var body: some View {
         content()
             .environmentObject(vm)
+            .onChange(of: vm.selection) { newValue in
+                selection = newValue as? ID
+            }
     }
 }
 
@@ -34,6 +37,34 @@ public enum DSPickerBuilder {
             ForEach(0..<components.count, id: \.self) { index in
                 DSRowPickerItem {
                     components[index]
+                }
+            }
+        }
+        .padding(.horizontal, ds: .medium)
+    }
+    
+    public static func buildBlock<ID, Data, Content>(
+        _ loop: ForEach<Data, ID, Content>
+    ) -> some View where ID: Hashable, Data: RandomAccessCollection, Data.Element: Identifiable, Content: View {
+        VStack(spacing: .ds.spacing.small) {
+            ForEach(loop.data) { data in
+                DSRowPickerItem {
+                    loop.content(data)
+                }
+                .withTag(data.id)
+            }
+        }
+        .padding(.horizontal, ds: .medium)
+    }
+    
+    public static func buildBlock<ID, Data, Content>(
+        _ loop: ForEach<Data, ID, Content>
+    ) -> some View where ID: Hashable, Data: RandomAccessCollection, Content: View {
+        VStack(spacing: .ds.spacing.small) {
+            ForEach(0..<loop.data.count, id: \.self) { index in
+                let data = loop.data[index as! Data.Index]
+                DSRowPickerItem {
+                    loop.content(data)
                 }
                 .withTag(index)
             }
@@ -48,18 +79,32 @@ class DSPickerSelectionViewModel: ObservableObject {
             print(selection)
         }
     }
-    
-    private var cancellables: Set<AnyCancellable> = .init()
 }
 
 struct DSPickerPreview: PreviewProvider {
+    @State static var selection: String? = "watermelon"
+    @State static var items = [
+        Fruit(name: "strawberry"),
+        Fruit(name: "watermelon"),
+        Fruit(name: "tomato")
+    ]
     static var previews: some View {
         ZStack {
             Background(.secondary)
-            DSPicker(selection: .constant(3)) {
-                Text("teste")
-                Text("teste")
+            DSPicker(selection: $selection) {
+                ForEach(items) { fruit in
+                    Text(fruit.name)
+                }
             }
         }
     }
+}
+
+public struct Fruit: Identifiable {
+    public init(name: String) {
+        self.name = name
+    }
+    
+    public var id: String { name }
+    public var name: String
 }
