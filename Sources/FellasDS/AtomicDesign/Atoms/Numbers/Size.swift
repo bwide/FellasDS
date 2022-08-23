cd7cb7cdd89b60a38d5b01a1b9e375d11adc57fa
Bruno Fulber Wide <bfwide07@gmail.com>
2022-08-23 15:23:48 -0300

    added xLarge size

--- Sources/FellasDS/AtomicDesign/Atoms/Numbers/Size.swift
+++ Sources/FellasDS/AtomicDesign/Atoms/Numbers/Size.swift
@@ -9,13 +9,23 @@ import Foundation
 import CoreGraphics
 
 public enum DSSize: CGFloat, CaseIterable {
+    /// 30
     case small = 30
+    /// 60
     case medium = 60
+    /// 90
     case large = 90
+    /// 180
+    case x_large = 180
 }
 
 public struct DSSizes {
+    /// 30
     public let small = DSSize.small.rawValue
+    /// 60
     public let medium = DSSize.medium.rawValue
+    /// 90
     public let large = DSSize.large.rawValue
+    /// 180
+    public let x_large = DSSize.x_large.rawValue
 }
