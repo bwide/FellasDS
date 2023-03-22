2da3fc9a3b47935acd8a82cf97b257d9813aa4a7
Bruno Fulber Wide <bfwide07@gmail.com>
2023-03-22 18:33:09 -0300

    updated DSPicker

---
+++ Sources/FellasDS/AtomicDesign/Organisms/Pickers/HorizontalPicker.swift
@@ -0,0 +1,74 @@
+//
+//  File.swift
+//  
+//
+//  Created by Bruno Fulber Wide on 20/03/23.
+//
+
+import Foundation
+import SwiftUI
+
+public struct HorizontalPickerStyle: DSPickerStyle {
+    public func makeBody(configuration: Configuration) -> some View {
+        ScrollView(.horizontal) {
+            HStack(spacing: .ds.spacing.medium) {
+                ForEach(configuration.views) { view in
+                    DSHorizontalPickerItem {
+                        view.body
+                    }
+                    .withTag(view.id)
+                }
+            }
+        }
+    }
+}
+
+struct DSHorizontalPickerItem<Content: View>: View, Taggable {
+        
+    @EnvironmentObject var vm: DSPickerSelectionViewModel
+    var tag: AnyHashable = UUID()
+    var content: () -> Content
+    
+    init(_ content: @escaping () -> Content) {
+        self.content = content
+    }
+    
+    var body: some View {
+        content()
+            .padding(ds: .xSmall)
+            .background(style: .secondary)
+            .cornerRadius(ds: .medium)
+            .overlay {
+                if selected {
+                    RoundedRectangle(cornerRadius: .ds.cornerRadius.medium)
+                        .strokeBorder(Color.ds.brand.tertiary, lineWidth: 3)
+                }
+            }
+            .onTapGesture { onTapGesture() }
+    }
+}
+
+extension DSHorizontalPickerItem {
+    
+    internal init(content: @escaping () -> Content,
+                  tag: AnyHashable) {
+        self.content = content
+        self.tag = tag
+    }
+    
+    func withTag(_ tag: AnyHashable) -> DSHorizontalPickerItem<Content> {
+        DSHorizontalPickerItem(content: content, tag: tag)
+    }
+}
+
+struct HorizontalPicler_Previews: PreviewProvider {
+    static var previews: some View {
+        DSPicker {
+            ForEach([1,2,3,4], id: \.self) { n in
+                Text("\(n)")
+            }
+        }
+        .padding(.horizontal, ds: .medium)
+        .dsPickerStyle(.horizontal)
+    }
+}
---
+++ Sources/FellasDS/AtomicDesign/Organisms/Pickers/PickerBuilder.swift
@@ -0,0 +1,49 @@
+//
+//  File.swift
+//  
+//
+//  Created by Bruno Fulber Wide on 21/03/23.
+//
+
+import Foundation
+import SwiftUI
+
+public struct TagView: View, Identifiable {
+    var label: AnyView
+    public var id: AnyHashable
+    
+    public var body: some View {
+        label
+    }
+}
+
+@resultBuilder
+public enum VerticalPickerBuilder {
+    public static func buildFinalResult(_ component: [TagView]) -> DSPickerStyleConfiguration {
+        .init(views: component)
+    }
+    
+    public static func buildBlock<ID: Hashable, Data: RandomAccessCollection, Content: View>(
+        _ loop: ForEach<Data, ID, Content>
+    ) -> [TagView] {
+        loop.data.enumerated().map {
+            TagView(label: AnyView(loop.content($0.element)), id: $0.offset)
+        }
+    }
+    
+    public static func buildBlock<ID: Hashable, Data: RandomAccessCollection, Content: View>(
+        _ loop: ForEach<Data, ID, Content>
+    ) -> [TagView] where Data.Element: Identifiable {
+        loop.data.map {
+            TagView(label: AnyView(loop.content($0)), id: $0.id)
+        }
+    }
+    
+    public static func buildBlock<Content: View>(
+        _ contents: Content...
+    ) -> [TagView] {
+        contents.enumerated().map {
+            TagView(label: AnyView($0.element), id: $0.offset)
+        }
+    }
+}
