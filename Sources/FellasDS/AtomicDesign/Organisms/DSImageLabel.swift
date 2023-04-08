84a62e8244b960b3d3573eeb1a9ee74924f236c3
Bruno Fulber Wide <bfwide07@gmail.com>
2023-04-08 23:43:47 +0200

    added DSImageLabel

---
+++ Sources/FellasDS/AtomicDesign/Organisms/DSImageLabel.swift
@@ -0,0 +1,47 @@
+//
+//  File.swift
+//  
+//
+//  Created by Bruno Fulber Wide on 04/04/23.
+//
+
+import Foundation
+import SwiftUI
+
+public struct DSAsyncImage: View {
+    
+    var url: URL?
+    
+    public init(url: URL? = nil) {
+        self.url = url
+    }
+    
+    public var body: some View {
+        AsyncImage(url: url) { phase in
+            DSAsyncImageContent(phase)
+        }
+    }
+}
+
+public struct DSAsyncImageContent: View {
+    
+    var phase: AsyncImagePhase
+    
+    public init(_ phase: AsyncImagePhase) {
+        self.phase = phase
+    }
+    
+    public var body: some View {
+        switch phase {
+        case .success(let image):
+            image
+                .resizable()
+                .padding(ds: .medium)
+                .scaledToFit()
+        case .failure(let error):
+            Text(error.localizedDescription)
+        default:
+            ProgressView()
+        }
+    }
+}
