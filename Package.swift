b1f3a2b34aa256e2c6eb2a5c915c2aabfe3232d3
Bruno Fulber Wide <bfwide07@gmail.com>
2023-10-27 16:46:58 -0300

    updated packages

--- Package.swift
+++ Package.swift
@@ -17,8 +17,8 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(path: "../../Packages/FellasLocalization"),
-        .package(path: "../../Packages/FellasStoreKit"),
+        .package(url: "https://github.com/bwide/FellasLocalization", from: "1.0.0"),
+        .package(url: "https://github.com/bwide/FellasStoreKit", from: "1.0.0"),
         .package(path: "../../Packages/SwiftResources")
     ],
     targets: [
